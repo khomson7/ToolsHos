e6754f414c1208aa1c79881046f58b7fb7287326
Tran <khomson7@gmail.com>
2021-01-06 05:34:29 +0700

    edit

--- modules/depress/controllers/DefaultController.php
+++ modules/depress/controllers/DefaultController.php
@@ -504,29 +504,29 @@ $this->exec_hos($sql2);
 
             curl_close($curl);
 
-            $curl = curl_init();
-
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => "$url/ppspecials/updateserial", //เปลี่ยนแปลง
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => "GET",
-                CURLOPT_POSTFIELDS => "",
-                CURLOPT_HTTPHEADER => array(
-                    "Authorization: Bearer $token",
-                    "Content-Type: application/json",
-                ),
-            ));
-
-            $response = curl_exec($curl);
-
-            curl_close($curl);
-
         }
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "$url/ppspecials/updateserial", //เปลี่ยนแปลง
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "GET",
+            CURLOPT_POSTFIELDS => "",
+            CURLOPT_HTTPHEADER => array(
+                "Authorization: Bearer $token",
+                "Content-Type: application/json",
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
 
         echo SweetAlert::widget([
             'options' => [
