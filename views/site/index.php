25e6c84cb0f669774605ac54a65bbaa68a99340e
Tran <khomson7@gmail.com>
2021-01-13 15:04:54 +0700

    edit130164

--- views/site/index.php
+++ views/site/index.php
@@ -34,15 +34,8 @@ yii\bootstrap4\Modal::end();
 
     <div class="jumbotron">
 
-        <div class="spinner-border text-muted"></div>
-        <div class="spinner-border text-primary"></div>
-        <div class="spinner-border text-success"></div>
-        <div class="spinner-border text-info"></div>
-        <div class="spinner-border text-warning"></div>
-        <div class="spinner-border text-danger"></div>
-        <div class="spinner-border text-secondary"></div>
-        <div class="spinner-border text-dark"></div>
-        <div class="spinner-border text-light"></div>
+        <div class="btn text-muted"></div>
+        
 
         <h1 class="text-primary"><strong>ยินดีต้อนรับเข้าสู่</strong></h1>
 
--- views/site/index2.php
+++ views/site/index2.php
@@ -12,15 +12,7 @@ $this->title = 'Tools-Hos';
 
      <div class="jumbotron">
         
-        <div class="spinner-border text-muted"></div>
-  <div class="spinner-border text-primary"></div>
-  <div class="spinner-border text-success"></div>
-  <div class="spinner-border text-info"></div>
-  <div class="spinner-border text-warning"></div>
-  <div class="spinner-border text-danger"></div>
-  <div class="spinner-border text-secondary"></div>
-  <div class="spinner-border text-dark"></div>
-  <div class="spinner-border text-light"></div>
+   
 
         <h1 class="text-primary"><strong>ยินดีต้อนรับเข้าสู่</strong></h1>
 
