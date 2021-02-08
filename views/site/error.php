2c90beae9fbd0fd8645319da6fc124ecf4ee48c4
Tran <khomson7@gmail.com>
2021-02-09 06:52:32 +0700

    edit090264

--- views/site/error.php
+++ views/site/error.php
@@ -6,15 +6,19 @@
 /* @var $exception Exception */
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 $this->title = $name;
 ?>
 <div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    
 
+    <div class="d-flex justify-content-center">
+      <h1><?=Html::img(Url::base().'/images/sql_error.png')?>
     <div class="alert alert-danger">
         <?= nl2br(Html::encode($message)) ?>
+    </div>
     </div>
 
     
